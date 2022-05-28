63a6de65f751e6cbbb56f84a040236908e781cb3
TMexe404 <muhammadrivan888@gmail.com>
2022-05-28 21:16:53 +0700

    second deploy

--- redcode/settings.py
+++ redcode/settings.py
@@ -127,12 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# for deploy
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Extra places for collectstatic to find static files.
+# for local
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
-# )
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
